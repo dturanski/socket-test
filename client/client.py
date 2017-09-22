6273d6b0e114e7c1bf93da47fb51b6ca2056bad3
David Turanski <dturanski@pivotal.io>
2017-09-22 12:05:24 -0400

    modify client

--- client/client.py
+++ client/client.py
@@ -1,7 +1,7 @@
 import socket
 import time
 import os
-
+import sys
 
 HOST, PORT = os.getenv('HOST',os.getenv('HOSTNAME','localhost')), 9999
 
@@ -9,11 +9,12 @@ HOST, PORT = os.getenv('HOST',os.getenv('HOSTNAME','localhost')), 9999
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
+    data = sys.argv[1] if len(sys.argv) >= 2  else 'hello'
     # Connect to server and send data
     print ("connecting to %s:%d" %(HOST, PORT))
     sock.connect((HOST, PORT))
     while 1:
-        sock.sendall("hello\n")
+        sock.sendall(data + '\n')
         time.sleep(1)
 
     # Receive data from the server and shut down
